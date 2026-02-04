b7472322928a0f41d94b095b8f313925862f0a03
1da1ana1 <yasmimdaiana10@gmail.com>
2026-02-04 17:24:24 -0300

    criada migrations e conexão com bano de dados, rotas estão funcionais

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata'
 import express from 'express';
 import './database';
-
+import { router } from './routes';
 
 const app = express();
 
-app.get("/users", (req, res) =>{
-    return res.json({ message: "Hello World" });
-})
-
-app.post("/", (req, res) =>{
-    return res.json({ message: "ebaaaaaaaa" });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
