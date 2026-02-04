46a24298025aea018722dd1f7f99d5ee21fc6c9d
1da1ana1 <yasmimdaiana10@gmail.com>
2026-02-04 18:11:00 -0300

    criado model de usuario, controller de usuario e rota

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
-
+import { AppDataSource } from "../database/data-source";
+import { User } from "../models/User";
 class UserController {
     async create(req: Request, res: Response) {
-        const body = req.body;
-        console.log(body);
-        return res.send()
+        const { name, email } = req.body;
+        
+        const userRepository = AppDataSource.getRepository(User);
+        const userAlreadyExists = await userRepository.findOneBy({ email });
+        
+        if (userAlreadyExists) {
+            return res.status(400).json({ message: "User already exists!" });
+        }
+        
+        const user = userRepository.create({        
+            name, email
+        })
+
+        await userRepository.save(user);
+        return res.json(user)
 
     }
 }
