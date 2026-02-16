fc6419945d0da4b9edc76d61c31a3b0d9ce6f9ad
1da1ana1 <yasmimdaiana10@gmail.com>
2026-02-16 16:24:40 -0300

    finalizando a aplicação com tratamento de erros

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,15 +2,26 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../database/data-source";
 import { User } from "../models/User";
 import UserRepository from "../repositories/UserRepository";
+import * as yup from "yup";
+import { AppErrors } from "../errors/AppErrors";
 class UserController {
     async create(req: Request, res: Response) {
         const { name, email } = req.body;
         
+        const schema = yup.object().shape({
+            name: yup.string().required("Name is required!"),
+            email: yup.string().email("Invalid email format!").required("Email is required!"),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+           throw new AppErrors("Validation fails!");
+        }
+
         const userRepository = AppDataSource.getRepository(User).extend(UserRepository);
         const userAlreadyExists = await userRepository.findOneBy({ email });
         
         if (userAlreadyExists) {
-            return res.status(400).json({ message: "User already exists!" });
+           throw new AppErrors("User already exists!");
         }
         
         const user = userRepository.create({        
