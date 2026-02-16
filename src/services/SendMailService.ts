648a2b5369b1ed406fb8cd556cc38c3ba383f1c7
1da1ana1 <yasmimdaiana10@gmail.com>
2026-02-16 13:27:05 -0300

    adicionado controller

---
+++ src/services/SendMailService.ts
@@ -0,0 +1,45 @@
+import nodemailer, { Transport } from "nodemailer";
+import Handlebars from "handlebars";
+import fs from "fs";
+
+
+class SendMailService {
+  private client: nodemailer.Transporter;
+  constructor() {
+    nodemailer
+      .createTestAccount()
+      .then((account) => {
+        const transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
+        this.client = transporter;
+      })
+      .catch((err) => {
+        console.error("Failed to create a testing account. " + err.message);
+      });
+  }
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const mailTemplateParse = Handlebars.compile(templateFileContent);
+    const html = mailTemplateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      html: html,
+      from: "NPS <noreplay@nps.com.br>",
+    });
+
+    console.log("Message sent: %s", message.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export default new SendMailService();
