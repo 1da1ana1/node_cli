b91dc84865ba9de2fd253b4286ea592ed61a92eb
1da1ana1 <yasmimdaiana10@gmail.com>
2026-02-16 15:35:11 -0300

    controller com o calculo do nps criado

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { UserController } from "./controllers/UserController";
 import { SurveysController } from "./controllers/SurveysController";
 import SendMailController  from "./controllers/SendMailController";
 import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
 
 const router = Router();
 
@@ -11,14 +12,16 @@ const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
 
 const answerController = new AnswerController();
-
-router.get("/answers/:value", answerController.execute);
+const npsController = new NpsController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
 
 
 export { router };
